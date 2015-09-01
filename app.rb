e7981024ca3ed206f3e1b0f900b2cddf4edcac59
rachael berecka & yelena nedelko <student@epicodus.com>
2015-09-01 14:59:48 -0700

    Add initial teachers files

---
+++ app.rb
@@ -0,0 +1,9 @@
+require("bundler/setup")
+Bundler.require(:default)
+
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+#teacher welcome page
+get('/') do
+    erb(:teacher_welcome)
+end
