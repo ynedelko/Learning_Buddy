6fe25a4a04c351190f393c2866886f071b8c84a5
Yelena Nedelko & Rachael Berecka <student@epicodus.com>
2015-09-02 09:43:03 -0700

    Add less to teacher-admin page

--- spec/teacher_integration_spec.rb
+++ spec/teacher_integration_spec.rb
@@ -12,4 +12,10 @@ describe("the route a teacher takes", {:type => :feature} ) do
         click_button('Add Student')
         expect(page).to have_content("Yan Polski")
     end
+    it("adds a new student to the roster") do
+        visit("/teacher-admin/")
+        fill_in('lesson_name', :with => "ABC")
+        click_button('Add Lesson')
+        expect(page).to have_content("ABC")
+    end
 end
